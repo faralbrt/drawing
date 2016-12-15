ffa6bca32a67bc82ce40fd300314dee9f7d5b143
Mark <m.tanca@hotmail.com>
2016-12-14 22:03:10 -0500

    added jquery to display data URI as img in html, added comments

--- application.js
+++ application.js
@@ -1,6 +1,23 @@
 /* © 2009 ROBO Design
  * http://www.robodesign.ro
  */
+ $(document).ready(function() {
+    $(".button").on("click", function(){
+      event.preventDefault()
+        var canvas = document.getElementById("imageView");
+        // line 12 returns user's image as dataURI... could just send to server
+        // and save in DB
+        var image = canvas.toDataURL("image/jpeg", 1.0);
+
+        // just renders user image for now... have to change some default
+        // settings like background-color(displays black)  
+        $("#imgCopy").attr("src", image)
+    })
+ });
+
+
+
+
 
 // Keep everything in anonymous function, called on window load.
 if(window.addEventListener) {
